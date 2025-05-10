ab9b713611a7e5b78559c08d4ac9b3038107332e
Soyeon Choe <soy2302ten@gmail.com>
2025-05-10 22:58:10 +0900

    feat: try-catch를 활용하여 예외상황 처리

--- 04-JavaScript-Async-Function/main.js
+++ 04-JavaScript-Async-Function/main.js
@@ -1,7 +1,12 @@
 const fetchUser = () => new Promise((resolve) => setTimeout(resolve, 2000, '홍길동'));
 
 const main = async () => {
-	console.log(await fetchUser());
+	try {
+		const userName = await fetchUser();
+		console.log(userName);
+	} catch (err) {
+		console.error('fetchUser 실패:', err);
+	}
 };
 
 main();
