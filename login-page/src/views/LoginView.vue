6e9090e800875ba610d2112386c333286998c626
Soyeon Choe <soy2302ten@gmail.com>
2025-05-15 13:30:57 +0900

    feat: 로그인 페이지 만들기

--- login-page/src/views/LoginView.vue
+++ login-page/src/views/LoginView.vue
@@ -1,6 +1,41 @@
 <template>
 	<div class="login-view">
-		<h1>로그인 페이지 입니다!</h1>
-		<router-link to="/">홈으로</router-link>
+		<img src="@/assets/imgs/wadiz.png" alt="로고" />
+		<v-input v-model="user.id"></v-input>
+		<v-input v-model="user.password"></v-input>
+		<v-button>로그인</v-button>
+		<v-button>회원가입</v-button>
 	</div>
 </template>
+
+<script>
+import VInput from '@/components/common/VInput.vue';
+import VButton from '@/components/common/VButton.vue';
+
+export default {
+	name: 'login-view',
+	components: {
+		VInput,
+		VButton,
+	},
+	data() {
+		return {
+			user: {
+				id: '',
+				password: '',
+			},
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.login-view {
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-flow: column;
+}
+</style>
